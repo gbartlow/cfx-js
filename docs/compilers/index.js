1498d4e66ef8bbfbff7290e1ebd82766d38ccf9c
Matt Balmer <mattbalmer@carfax.com>
2015-04-28 11:41:00 -0500

    updated docs. merge in 0.2.4

--- docs/compilers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    article: require('./article'),
-    aside: require('./aside'),
-    src: require('./src'),
-    tests: require('./tests'),
-    markdown: require('./markdown')
-};
